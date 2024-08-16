4d8298fdb61f4daaea0bf289390e540088513ff0
Sultan-html <shalava1321@gmail.com>
2024-08-16 18:13:37 +0600

    cod

---
+++ fds.py
@@ -0,0 +1,21 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        if not words:
+            return []
+        
+       
+        common_count = Counter(words[0])
+        
+        for word in words[1:]:
+            current_count = Counter(word)
+            common_count &= current_count
+        
+       
+        result = []
+        for char, count in common_count.items():
+            result.extend([char] * count)
+        
+        return result
